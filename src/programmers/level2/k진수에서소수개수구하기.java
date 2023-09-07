be014ff70bc76e5e83c0e1b35c235950ae594d83
naegonggae <tkdtkd97@naver.com>
2023-09-07 21:15:10 +0900

    [PRG] K진수에서 소수 개수 구하기

---
+++ src/programmers/level2/k진수에서소수개수구하기.java
@@ -0,0 +1,10 @@
+package programmers.level2;
+
+public class k진수에서소수개수구하기 {
+
+	public static void main(String[] args) {
+		String num = Integer.toString(1000000, 2);
+		System.out.println(num);
+	}
+
+}
